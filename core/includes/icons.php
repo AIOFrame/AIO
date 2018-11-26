894e55a8e3d151c86be1458163b327e9f14afb58
Shaikh Moinuddin <shaikh@homebazar.com>
2018-11-26 09:55:40 +0400

    More Icons

--- core/includes/icons.php
+++ core/includes/icons.php
@@ -5,7 +5,7 @@ function icon( $name, $size = 25, $thickness = 15, $color = '#000000', $stroke =
     if( file_exists( COREPATH . 'core/components/icons/'. $name . '.svg' ) ) {
         $icon++;
         $stroke = $stroke ? 'none' : $color;
-        echo '<div style="width:' . $size . 'px;height:' . $size . 'px;stroke-width:' . $thickness . ' !important;stroke:' . $color . ' !important;fill:' . $stroke . ' !important">';
+        echo '<div class="ico" style="width:' . $size . 'px;height:' . $size . 'px;stroke-width:' . $thickness . ' !important;stroke:' . $color . ' !important;fill:' . $stroke . ' !important">';
         echo '<style>.</style>';
         $f = file_get_contents( COREPATH . 'core/components/icons/' . $name . '.svg');
         /* $f = preg_replace('@<(style)\b.*?>.*?</\1>@si', '', $f); */
