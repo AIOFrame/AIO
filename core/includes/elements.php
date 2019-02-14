ad6fe26333432ddb54feac6c242e56e57f4c67a8
Shaikh <shaikh@Shaikhs-iMac.local>
2019-02-14 01:26:26 -0800

    Added ACE Code Editor

--- core/includes/elements.php
+++ core/includes/elements.php
@@ -19,3 +19,26 @@ function body_class() {
     $pc = str_replace('/',' ',PAGEPATH);
     echo 'class="'.$dc.' '.$pc.'"';
 }
+
+function ace( $element_id, $mode = 'html' ) {
+    echo '<script src="'.APPURL.'core/components/ace/ace.min.js" type="text/javascript" charset="utf-8"></script>'.
+    '<script src="'.APPURL.'core/components/ace/emmet.min.js"></script>'.
+    '<script src="'.APPURL.'core/components/ace/ext-language_tools.min.js"></script>'.
+    '<script>
+        document.addEventListener("DOMContentLoaded", function(e) {
+            
+            // Load Scripts
+            ace.config.set("basePath", "'.APPURL.'core/components/ace");
+            
+            var editor = ace.edit("' . $element_id . '");
+            editor.setTheme("ace/theme/twilight");
+            
+            editor.session.setMode("ace/mode/'.$mode.'");
+            editor.setOptions({
+                enableBasicAutocompletion: true,
+                enableSnippets: true,
+                enableLiveAutocompletion: false
+            });
+        });
+    </script>';
+}
