e125c4eb13b7d9dd85ef160e3ae219060ddd2b48
shaikh@utsme.com <Shaikh1234>
2019-02-26 13:08:21 +0400

    Favicon Fix

--- core/includes/elements.php
+++ core/includes/elements.php
@@ -2,13 +2,13 @@
 
 function favicon( $icon ){
     if( file_exists( APPPATH . 'assets/images/' . $icon . '.png' ) ) {
-        $link = APPURL . 'apps/' . APPNAME . '/assets/images/'.$icon.'.png';
+        $link = APPURL . 'apps/' . APPDIR . '/assets/images/'.$icon.'.png';
         echo '<link rel="shortcut icon" href="'.$link.'">';
         if (is_mobile()) {
             echo file_exists( APPPATH . 'assets/images/' . $icon . '.png' ) ? '<link rel="apple-touch-icon" href="'.$link.'"/>' : '';
             $sizes = [144,114,72,57];
             foreach( $sizes as $s ){
-                echo file_exists( APPPATH . 'assets/images/' . $icon . '-' . $s . '.png' ) ? '<link rel="apple-touch-icon" href="'.APPURL . 'apps/' . APPNAME . 'assets/images/'.$icon.'-'.$s.'.png"/>' : '';
+                echo file_exists( APPPATH . 'assets/images/' . $icon . '-' . $s . '.png' ) ? '<link rel="apple-touch-icon" href="'.APPURL . 'apps/' . APPDIR . 'assets/images/'.$icon.'-'.$s.'.png"/>' : '';
             }
         }
     }
