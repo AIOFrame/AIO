61a3f4005d24daa5721c4323122ade02e3ad8ffc
Shaikh <shaikh@Shaikhs-iMac.local>
2019-03-14 09:38:38 +0400

    svalid_email and slength added

--- assets/scripts/core.js
+++ assets/scripts/core.js
@@ -459,6 +459,23 @@ function clear(e){
     $(e).val("");
 }
 
+function valid_email( id ) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return !pattern.test( $(id).val() );
+}
+
+function svalid_email( id ) {
+    var v = valid_email( id );
+    !v ? $(id).removeClass('empty') : $(id).addClass('empty');
+    return v;
+}
+
+function slength( e, l ){
+    var r = $(e).val().length > l;
+    r ? $(e).removeClass('empty') : $(e).addClass('empty');
+    return !r;
+}
+
 function get_values( e, s, pre ) {
 
     pre = pre !== undefined && pre !== '' ? pre + '_' : '';
