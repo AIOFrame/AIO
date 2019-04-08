29a6c372b25b8a10063da803c1db4aca71451b93
Shaikh <shaikh@Shaikhs-iMac.local>
2019-04-09 03:54:48 +0400

    SET NAMES utf8

--- core/core.php
+++ core/core.php
@@ -107,11 +107,12 @@ function app_loader() {
 
         if( isset( $c['host'] ) && !empty( $c['host'] ) && isset( $c['user'] ) && !empty( $c['user'] ) && isset( $c['pass'] ) && !empty( $c['pass'] ) && isset( $c['database'] ) && !empty( $c['database'] ) ){
 
-            header("Content-Type: text/html; charset=UTF-8");
+            //header("Content-Type: text/html; charset=UTF-8");
 
             global $db;
             $db = @mysqli_connect( $c['host'], $c['user'], $c['pass'], $c['database'] );
             if ( $db ) {
+                mysqli_query( $db, "SET NAMES 'utf8'");
                 mysqli_query( $db, 'SET CHARACTER SET utf8' );
                 !defined( 'APPCON' ) ? define( 'APPCON', true ) : '';
             } else {
