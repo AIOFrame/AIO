93e4220580583176cc3feb50d99c955553712701
AIO <shaikh@aioframe.com>
2022-04-07 15:02:07 +0400

    Log Structure Added

--- core/includes/structure.php
+++ core/includes/structure.php
@@ -12,7 +12,21 @@ $option_struct = [
     [ 'load', 'BOOLEAN', '', 0 ],
 ];
 $db = new DB();
-$db->create_table( 'options', $option_struct, 'option', 1 );
+$db->automate_tables( [ 'options', $option_struct, 'option', 1 ] );
+
+$log_struct[] = [ 'log', [
+    [ 'datetime', 'DATETIME', '', 1 ],
+    [ 'change', 'VARCHAR', 64, 1 ],
+    [ 'table', 'VARCHAR', 64, 1 ],
+    [ 'data', 'VARCHAR', 4096, 0 ],
+    [ 'where', 'VARCHAR', 2048, 0 ],
+    [ 'url', 'VARCHAR', 512, 0 ],
+    [ 'line', 'INT', 13, 0 ],
+    [ 'uid', 'INT', 13, 0 ],
+    [ 'name', 'VARCHAR', 256, 0 ],
+], 'log', 1 ];
+$db = new DB();
+$db->automate_tables( $log_struct );
 
 /**
  * Get App Config and create database tables
@@ -32,6 +46,11 @@ if( is_array( $feats ) ) {
 
     // Create File uploader tables if featured
     if( in_array( 'storage', $feats ) || in_array( 'uploads', $feats ) ) {
+        include_once( $path . 'storage.php' );
+    }
+
+    // Create File uploader tables if featured
+    if( in_array( 'log', $feats ) || in_array( 'uploads', $feats ) ) {
         include_once( $path . 'storage.php' );
     }
 
