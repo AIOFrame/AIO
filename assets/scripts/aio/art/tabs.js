7ed652e04b7a84cf3d69c2626e18941e90f8e3f9
AIO <shaikh@aioframe.com>
2022-11-02 11:03:56 +0400

    Minor Update to Tabs js

--- assets/scripts/aio/art/tabs.js
+++ assets/scripts/aio/art/tabs.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
 
-    let page_path = location.pathname.replace( '/', '_' ).slice(1);
-    let tab = localStorage[ page_path + '_tab'];
-    if( tab !== undefined ){
-        setTimeout(function(){ $('[data-t="' + tab + '"]').click(); },200);
-    }
+    let page_path = location.pathname.replace( '/', '_' ).slice(1) + '_';
+    $('[data-store]').each(function(){
+        let title = $(this).data('store') !== '' ? $(this).data('store') + '_' : '_';
+        let tab = localStorage[ page_path + title + 'tab'];
+        if( tab !== undefined ){
+            setTimeout(function(){ $('[data-t="' + tab + '"]').click(); },200);
+        }
+    });
 
     $('body').on('click', '.steps [data-t],.tabs [data-t]', function () {
         $(this).parent().children().removeClass('on');
         $(this).addClass('on');
         $($(this).data('t')).parent().children().hide();
         $($(this).data('t')).show();
-        if( $(this).parent().data('store') !== undefined || $(this).parent().data('save') !== undefined ){
-            localStorage[ page_path + '_tab' ] = $(this).data('t');
+        if( $(this).parent().data('store') !== undefined ){
+            let title = $(this).parent().data('store') !== '' ? $(this).parent().data('store') + '_' : '_';
+            localStorage[ page_path + title + 'tab' ] = $(this).data('t');
+        }
+        if( $(this).parent().data('save') !== undefined ){
+            let title = $(this).parent().data('save') !== '' ? $(this).parent().data('save') + '_' : '_';
+            localStorage[ page_path + title + 'tab' ] = $(this).data('t');
         }
     })
     // Next Step
