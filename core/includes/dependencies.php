f4c9f6d34ce29131af420b25ed2abe251f60c197
AIOFrame <shaikh@aioframe.com>
2022-11-29 08:03:48 +0400

    Update dependencies.php

--- core/includes/dependencies.php
+++ core/includes/dependencies.php
@@ -77,10 +77,12 @@ function file_upload(): void {
  * @return void
  */
 function render_image( string|null $image = '', string $name = '', string $class = '', string $element = 'div' ): void {
-    //skel( !file_exists( APPPATH . $logo ) );
-    $n = ''; //!file_exists( APPPATH . $image ) && !empty( $name ) ? '<span>'.$name[0].'</span>' : '';
-    $image = !empty( $image ) ? ( $element == 'div' ? ' style="background-image:url(\''.storage_url( $image ).'\')"' : storage_url( $image ) ) : '';
-    echo $element == 'div' ? '<div class="image '.$class.'"'.$image.'>'.$n.'</div>' : '<img class="'.$class.'" src="'.$image.'" alt="'.$name.'">';
+    if( !empty( $image ) ) {
+        //skel( !file_exists( APPPATH . $logo ) );
+        $n = ''; //!file_exists( APPPATH . $image ) && !empty( $name ) ? '<span>'.$name[0].'</span>' : '';
+        $image = $element == 'div' ? ' style="background-image:url(\''.storage_url( $image ).'\')"' : storage_url( $image );
+        echo $element == 'div' ? '<div class="image '.$class.'"'.$image.'>'.$n.'</div>' : '<img class="'.$class.'" src="'.$image.'" alt="'.$name.'">';
+    }
 }
 
 function storage_url( $url = '' ): string {
