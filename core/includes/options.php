2e1dca384c38d8c6116512d6a05cf42f129f59b0
Shaikh Moinuddin <shaikh@aioframe.com>
2023-06-20 16:58:27 +0400

    Color Options Improv

--- core/includes/options.php
+++ core/includes/options.php
@@ -119,8 +119,8 @@ class OPTIONS {
         $colors = $this->colors;
         $ops = $db->get_options( $colors );
         $f->option_params_wrap( 'colors', 2, 2, $colors );
-        foreach( $colors as $c ) {
-            $f->color($c,ucwords(str_replace('_',' ',$c)),'Ex: F1F1F1',$ops[$c]??'',$attr,3,'','[data-key='.$c.']');
+        foreach( $colors as $c => $cv ) {
+            $f->color($c,ucwords(str_replace('_',' ',$c)),'Ex: F1F1F1',$ops[$c]??$cv,$attr,3,'','[data-key='.$c.']');
         }
         $f->process_options('Save Color Options','store grad','','.col-12 tac');
         echo '</div>';
