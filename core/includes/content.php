a14b4513fdb4fb012a4745f1519c1b7628d65bd6
Shaikh Moinuddin <shaikh@aioframe.com>
2023-07-04 15:29:17 +0400

    Minor Fixes

--- core/includes/content.php
+++ core/includes/content.php
@@ -285,6 +285,61 @@ class CONTENT {
 
 }
 
+class RANGE {
+
+    /**
+     * Returns array of 12 Months
+     * @param bool $assoc Whether to return indexed months or associative months
+     * @return array
+     */
+    function months( bool $assoc = true ): array {
+        return $assoc ? [ '01' => 'January', '02' => 'February', '03' => 'March', '04' => 'April', '05' => 'May', '06' => 'June', '07' => 'July', '08' => 'August', '09' => 'September', '10' => 'October', '11' => 'November', '12' => 'December' ] : [ 'January' => 'January', 'February' => 'February', 'March' => 'March', 'April' => 'April', 'May' => 'May', 'June' => 'June', 'July' => 'July', 'August' => 'August', 'September' => 'September', 'October' => 'October', 'November' => 'November', 'December' => 'December' ];
+    }
+
+    /**
+     * Returns array of range of years
+     * @param int $from From year
+     * @param int $to To year
+     * @param bool $desc Reverses the order of years
+     * @return array
+     */
+    function years( int $from, int $to, bool $desc = true ): array {
+        if( is_numeric( $from ) && is_numeric( $to )) {
+            $years = [];
+            if( strlen( $from ) > 3 && strlen( $to ) > 3 && $from < $to ) {
+                if( $desc ) {
+                    for ($x = $from; $x <= $to; $x++) {
+                        $years[] = $x;
+                    }
+                } else {
+                    for ($x = $to; $x >= $from; $x--) {
+                        $years[] = $x;
+                    }
+                }
+            } else {
+                for( $x = date('Y', strtotime('-'.$from.' years')); $x <= date('Y', strtotime('+'.$to.' years')); $x++){
+                    $years[] = $x;
+                }
+            }
+            return $years;
+        } else {
+            return [];
+        }
+    }
+
+    /**
+     * Years from given date till now
+     * @param string $date Date to start counting years till now
+     * @return false|string
+     */
+    function years_from_date( string $date ): false|string {
+        $then = date('Ymd', strtotime( $date ));
+        $diff = date('Ymd') - $then;
+        return substr($diff, 0, -4);
+    }
+
+}
+
 /**
  * Displays a NO ACCESS content and end further code execution
  * @param string $message Message to be displayed
@@ -304,4 +359,62 @@ function no_content( string $message = "No data found!", string $suggestion = ""
 
 function store_template_ajax(): void {
     elog( $_POST );
+}
+
+function __fn( $num, $decimals = 2, $locale = 'AE' ): void {
+    echo _fn( $num, $decimals, $locale );
+}
+
+function _fn( $num, $decimals = 2, $locale = 'AE' ): string {
+    $fmt = new NumberFormatter($locale = 'en_'.$locale, NumberFormatter::CURRENCY);
+    $fmt->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $decimals);
+    return str_replace( 'AED', '', $fmt->format( $num ) );
+}
+
+function nth( $num ): void {
+    $fmt = new NumberFormatter($locale = 'en', 6);
+    echo $fmt->format( $num );
+}
+
+function _nth( $num ): string {
+    $fmt = new NumberFormatter($locale = 'en', 6);
+    return $fmt->format( $num );
+}
+
+/**
+ * Returns a readable format of programming code
+ * @param string $text Code string
+ * @return string
+ */
+function _pre( string $text ): string {
+    return str_replace( '<', '&lt;', str_replace( '>', '&gt;', $text ) );
+}
+
+/**
+ * Echos a readable format of programming code
+ * @param string $text Code string
+ * @return void
+ */
+function pre( string $text ): void {
+    echo _pre( $text );
+}
+
+function easy_date( $date = '', $format = '', bool $show_time = false ): string {
+    if( $date == '' ){
+        $date = date('Y-m-d H:i:s');
+    }
+    $date = date_create( $date );
+    if( empty( $format ) ) {
+        global $options;
+        $format = $options['date_format'] ?? '';
+        if( empty( $format ) ) {
+            $c = json_decode(CONFIG, 1);
+            $format = is_array($c) && isset($c['date_format']) ? $c['date_format'] : 'd M, Y';
+        }
+    }
+    return date_format( $date, $format );
+}
+
+function easy_dt( $datetime = '', $format = 'd M, Y H:i a' ): string {
+    return easy_date( $datetime, $format );
 }
