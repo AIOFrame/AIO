9bb137cc403b333d0b633a5cfdb6ad5da92c6435
AIOFrame <shaikh@aioframe.com>
2024-06-02 12:29:00 +0400

    Table checkbox improv

---
+++ assets/scripts/aio/art/table.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $('body').on('click','table [data-toggle-checks]',function () {
+        let n = $(this).parent().index();
+        $(this).parents('table').find( 'tbody td:nth-child(' + ( n + 1 ).toString() + ') [type=checkbox]' ).attr('checked',$(this).is(':checked'));
+    })
+
+})
