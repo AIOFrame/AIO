55c31d6e2c3809724464a310b3c5d0a2768355b1
Shaikh Moinuddin <shaikh@aioframe.com>
2025-05-29 21:58:22 +0400

    Small Fix to DB Queries

--- core/includes/connect/queries.php
+++ core/includes/connect/queries.php
@@ -244,6 +244,7 @@ class DB {
                                 } else if( in_array( $type, [ 'phone', 'call' ] ) ) {
                                     $id2 = $row['identity2'] ?? ( $row['id2'] ?? ( $row['i2'] ?? '' ) );
                                     $r2 = $row['required2'] ?? ( $row['req2'] ?? ( $row['r2'] ?? 0 ) );
+                                    $table_structure[1][] = [ $id, 'VARCHAR', 6, $required ];
                                     if( !empty( $id2 ) ) {
                                         $table_structure[1][] = [ $id2, 'VARCHAR', 24, $r2 ];
                                     }
